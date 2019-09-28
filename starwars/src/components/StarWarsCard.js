f3c2fb377286a7e93532ab1a38c5ab37973ab276
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-28 10:17:42 -0700

    Fetched The Data

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,1 +1,9 @@
 import React from 'react';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle, Col, Button } from "reactstrap";
+
+export default function StarWarsCard(props) {
+
+    return(
+
+    )
+} 
